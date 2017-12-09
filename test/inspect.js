525f447f1e8f842613c5aff41e926d469420709c
sharaseaman <shara.seaman@gmail.com>
2017-12-09 12:21:28 -0600

    mocha

---
+++ test/inspect.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var expect = require('chai').expect;
+
+
+describe('fizzBuzz', function(){
+    it('returns fizz if 3', function(){
+        expect(3).to.equal(3);
+    });
+
+    it('returns buzz if 5')
+
+    it('returns fizzBuzz if 3 or 5')
+
+});
